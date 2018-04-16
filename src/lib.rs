2ab6f9df358e8cae7a985e5c8abbccccf8e5e990
George Kaplan <gkaplan@nu-tek-optics.com>
2018-04-16 18:02:02 -0400

    actually passing the closure from .execute() to the workers

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub struct ThreadPool {
     sender: mpsc::Sender<Job>,
 }
 
-struct Job;
+type Job = Box<FnOnce() + Send + 'static>;
 
 impl ThreadPool {
 
@@ -41,7 +41,9 @@ impl ThreadPool {
         where
             F: FnOnce() + Send + 'static
     {
-        //
+        let job = Box::new(f);
+        
+        self.sender.send(job).unwrap();
     }
 }
 
